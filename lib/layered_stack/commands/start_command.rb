fd7a1b0c85f6b2663a0e8e3f97da23f4b9d311d2
Tom Gallagher <tom.gallagher@theappengineers.com>
2024-09-05 20:31:10 +0100

    0.0.3

--- lib/layered_stack/commands/start_command.rb
+++ lib/layered_stack/commands/start_command.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'thor'
 require 'logger'
 
@@ -5,6 +7,9 @@ module LayeredStack
   module Cli
     module Commands
       class StartCommand < Thor
+        FRONTEND_DIR = "frontend"
+        LOCALHOST_URL = "http://localhost:3001"
+
         def self.execute
           new.execute
         end
@@ -26,36 +31,36 @@ module LayeredStack
           end
 
           def change_directory
-            if Dir.exist?("frontend")
+            if Dir.exist?(FRONTEND_DIR)
               begin
-                Dir.chdir("frontend")
-                logger.info("Changed directory to 'frontend'")
+                Dir.chdir(FRONTEND_DIR)
+                logger.info("Changed directory to '#{FRONTEND_DIR}'")
                 true
               rescue => e
                 logger.error("Failed to change directory: #{e.message}")
                 false
               end
             else
-              logger.error("Directory 'frontend' does not exist")
+              logger.error("Directory '#{FRONTEND_DIR}' does not exist")
               false
             end
           end
 
           def open_browser
             logger.info("\n# Opening the browser")
-            if system("open http://localhost:3001")
-              logger.info("Browser opened successfully")
-            else
-              logger.error("Failed to open the browser")
-            end
+            run_command("open #{LOCALHOST_URL}", "Browser opened successfully", "Failed to open the browser")
           end
 
           def run_development_server
             logger.info("\n# Running the development server")
-            if system("npm run dev")
-              logger.info("Development server started successfully")
+            run_command("npm run dev", "Development server started successfully", "Failed to start the development server")
+          end
+
+          def run_command(command, success_message, error_message)
+            if system(command)
+              logger.info(success_message)
             else
-              logger.error("Failed to start the development server")
+              logger.error(error_message)
             end
           end
         end
